df61d62edc338e39909cec21a7c307c2fbfa2c9f
Stefan Wienert <info@stefanwienert.de>
2024-09-24 12:35:44 +0000

    Fix: Loading issue

--- lib/static_error_pages/railtie.rb
+++ lib/static_error_pages/railtie.rb
@@ -1,13 +1,14 @@
 module StaticErrorPages
   class Railtie < ::Rails::Engine
-    # generators do
-      # require "path/to/my_railtie_generator"
-    # end
     config.static_error_pages = ActiveSupport::OrderedOptions.new
     config.static_error_pages.pages = %w(404.html 500.html 503.html 422.html)
     config.static_error_pages.layout = 'error_pages'
     config.to_prepare do
-      ApplicationController.helper(ErrorPagesHelper)
+      ActiveSupport.on_load(:action_controller) do
+        ActiveSupport::Reloader.to_prepare do
+          ApplicationController.helper(StaticErrorPages::ErrorPagesHelper)
+        end
+      end
     end
   end
 end
